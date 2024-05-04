c203a65057c4159d18dfa92fcd7ce25fdf098113
Astroira <122676563+Astroira@users.noreply.github.com>
2024-05-04 18:49:18 +0300

    Add files via upload
    v 1.0

---
+++ winner_generator.py
@@ -0,0 +1,36 @@
+#подключение библиотек
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from PyQt5.QtGui import * 
+from random import randint
+#создание элементов интерфейса
+app =QApplication([])
+my_win = QWidget()
+my_win.setWindowTitle('Лотерея')
+my_win.move(100,100)
+my_win.resize(400,200)
+#привязка элементов к вертикальной линии
+
+button = QPushButton('Cгенерировать')
+text = QLabel('Нажми, чтобы узнать победителя')
+rnum = QLabel('?')
+rnum.setFont(QFont('Arial', 50)) 
+
+line = QVBoxLayout()
+line.addWidget(text, alignment = Qt.AlignCenter)
+line.addWidget(rnum, alignment = Qt.AlignCenter)
+line.addWidget(button, alignment = Qt.AlignCenter)
+my_win.setLayout(line)
+#обработка событий
+def show_winner():
+    x = randint(1, 100)
+    rnum.setText(str(x))
+    text.setText('Победитель:')
+
+
+
+
+button.clicked.connect(show_winner)
+#запуск приложения
+my_win.show()
+app.exec_()
